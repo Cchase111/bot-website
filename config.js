19106bdea6ab1383479d272ebe075046cdd545f1
A7MED6969 <iamahmed3031@gmail.com>
2022-04-15 00:54:10 +0200

    commit

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     bot: {
-        owners: ["744183251102335007" , "" , ""],// owners id
-        server: "935661973356552215",// bot server id
-        botID : "901449764745388063",// bot id
-        clientSECRET: "nM2goT3SwXeYRIoNTbxfjCGfMPou1XEI",// bot secret
-        callbackURL: "http://localhost:3001/login",//website url (dont remove /login)
-        TOKEN : "OTAxNDQ5NzY0NzQ1Mzg4MDYz.YXQChA.tLxXI3ge6U2Emtiit9NYZtIRLAU",// bot token
+        owners: ["id1" , "id2" , "id3"],// owners id
+        server: "",// bot server id
+        botID : "",// bot id
+        clientSECRET: "",// bot secret
+        callbackURL: "/login",//website url (dont remove /login)
+        TOKEN : "",// bot token
     },
     channels: {
-        login: "960715167635763260",// logs room id
+        login: "",// logs room id
     },
     website: {
         PORT: "3001",//website port
