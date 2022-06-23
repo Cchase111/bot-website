0e4a5977eff82a61ac1ce1ffc5edabb1b02d6d38
Cchase111 <48870411+Cchase111@users.noreply.github.com>
2022-06-23 16:02:42 -0400

    wordle

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     bot: {
-        owners: ["id1" , "id2" , "id3"],// owners id
-        server: "",// bot server id
-        botID : "",// bot id
-        clientSECRET: "",// bot secret
+        owners: ["428918787463708674" , "id2" , "id3"],// owners id
+        server: "862429252078403614",// bot server id
+        botID : "742413729546633217",// bot id
+        clientSECRET: "19gFJLStb3DED5Ch1msQVGsrXD34bk3y",// bot secret
         callbackURL: "/login",//website url (dont remove /login)
-        TOKEN : "",// bot token
+        TOKEN : "NzQyNDEzNzI5NTQ2NjMzMjE3.XzFwqg.MLlKvVAm8-YubYVhs0ySEZLjFSE",// bot token
     },
     channels: {
-        login: "",// logs room id
+        login: "961811583414202408",// logs room id
     },
     website: {
         PORT: "3001",//website port
